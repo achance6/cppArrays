ccd6ca72c5132a06a349a671d78944b94733fc6e
achance6 <achance6@jhu.edu>
2019-11-13 14:40:59 -0500

    Now pre-initializes entire array and renamed

---
+++ ListArray.h
@@ -0,0 +1,29 @@
+#ifndef SIMPLEARRAY_H
+#define SIMPLEARRAY_H
+#include "Array.h"
+
+template<typename T>
+class ListArray : Array<T> {
+public:
+    ListArray(int length, T def);
+    T get(int position);
+    void put(int position, T data);
+    int length() { return len; }
+private:
+    T def;
+    class Node {
+    public:
+        Node(T data = T(), Node* next = nullptr, int position = -1) : data(data), next(next), position(position) {}
+        T data;
+        Node* next;
+        int position;
+    };
+    int len;
+    Node* head;
+    Node* find(int position);
+    void prepend(T data);
+};
+
+#include "ListArray.inc"
+#endif 
+
